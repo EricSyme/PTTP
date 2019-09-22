77f71ae09f2b9dd0cd366fbe35951e04af773e5a
Eric Syme <spotlightphotoseattle@gmail.com>
2019-09-22 12:24:35 -0700

    combining reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,17 @@
-import {createStore} from 'redux';
-import { Reducer, initialState } from './reducer'
+import {createStore, combineReducers} from 'redux';
+import { Recordings } from './recordings';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders';
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer, // reducer
-        initialState, // our initialState
+        combineReducers({
+            recordings: Recordings,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+        })
     );
 
     return store;
---
+++ src/redux/recordings.js
@@ -0,0 +1,8 @@
+import { RECORDINGS } from '../shared/recordings';
+
+export const Recordings = (state = RECORDINGS, action) => {
+    switch (action.type) {
+        default:
+          return state;
+      }
+};
